9cd2fabe1a5d8c6644904bd16a16ede223472291
Navn <152978422+OmerEryigit7@users.noreply.github.com>
2024-09-18 21:07:04 +0200

    Create Post (in admin panell)
    Lagde en liten modell på models.py som definerer hva "post" er. Så la jeg til "post" på admin-panelet på admin.py.

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    body = models.TextField()
+
+
+    def __str__(self):
+        return str(self.title) + '  -  ' + str(self.author)
 # Create your models here.
