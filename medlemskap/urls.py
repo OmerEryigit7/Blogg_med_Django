20b0d601c98b4f882f3308de5c22b78bd2cb1353
Navn <152978422+OmerEryigit7@users.noreply.github.com>
2024-10-28 15:12:08 +0100

    Man kan logge ut (snart)

--- medlemskap/urls.py
+++ medlemskap/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from .views import register
+from .views import LogoutView
 
 urlpatterns = [
-    path('lag_bruker/', register.as_view(), name='sign_up')
+    path('lag_bruker/', register.as_view(), name='sign_up'),
+    path('logg_ut/', LogoutView.as_view(), name='logout_confirmation')
 ]
--- medlemskap/views.py
+++ medlemskap/views.py
@@ -1,10 +1,22 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView, CreateView
+from django.views.generic import ListView, DetailView, CreateView, View
 from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
+from django.contrib.auth import logout
+from django.shortcuts import redirect
 
 # Create your views here.
 class register(CreateView):
     form_class = UserCreationForm
     template_name = 'registration/sign_up.html'
     success_url = reverse_lazy('login')
+
+class LogoutView(View):
+    template_name = 'registration/logout_confirmation.html'
+
+    def post(self, request, *args, **kwargs):
+        logout(request)  # Log out the user
+        return redirect('home')  # Redirect to the home page after logging out
+
+    def get(self, request, *args, **kwargs):
+        return render(request, self.template_name)  # Render the logout confirmation page
