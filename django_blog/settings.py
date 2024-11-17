e06b36500dad1af9c53a7e1f7ea8c7af184c4fce
Navn <152978422+OmerEryigit7@users.noreply.github.com>
2024-11-17 21:06:40 +0100

    gikk fra python 3.11 til 3.10.5
    For at det skal funke med pythonanywhere

--- django_blog/settings.py
+++ django_blog/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["127.0.0.1", "localhost", "omer7.pythonanywhere.com"]
  
@@ -133,6 +133,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'blog/static/style.css/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
