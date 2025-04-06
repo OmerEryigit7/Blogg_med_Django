30b131699ebb0e32066e1becf98f4a983d9a01c7
Navn <152978422+OmerEryigit7@users.noreply.github.com>
2025-04-06 17:36:29 +0200

    La til omertest.eu i allowed_hosts
    La til omertest.eu i allowed_hosts for å hoste.

--- django_blog/settings.py
+++ django_blog/settings.py
@@ -35,7 +35,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["127.0.0.1", "localhost", "omer7.pythonanywhere.com"]
+ALLOWED_HOSTS = ["127.0.0.1", "localhost", "omertest.eu"]
  
 # Application definition
 
